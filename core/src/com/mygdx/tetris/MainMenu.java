ad7e86921849e44fb3f83e1def2a7c4b58be0182
Joao611 <joaofvde@gmail.com>
2017-05-12 20:02:39 +0100

    initial commit

---
+++ core/src/com/mygdx/tetris/MainMenu.java
@@ -0,0 +1,26 @@
+package com.mygdx.tetris;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by joaof on 05/05/2017.
+ */
+
+class MainMenu extends ScreenAdapter {
+
+    private TetrisGame game;
+
+    public MainMenu(TetrisGame tetrisGame) {
+        game = tetrisGame;
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
