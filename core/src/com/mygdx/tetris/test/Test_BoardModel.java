27dfbf896146cfd91686c4b8610d1787129c3e2f
joao611 <joaofvde@gmail.com>
2017-05-16 17:32:59 +0100

    Added test structure

---
+++ core/src/com/mygdx/tetris/test/Test_BoardModel.java
@@ -0,0 +1,16 @@
+package com.mygdx.tetris.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by up201505145 on 16/05/2017.
+ */
+
+public class Test_BoardModel {
+    @Test
+    public void test1() {
+        assertEquals(1,1);
+    }
+
+}
