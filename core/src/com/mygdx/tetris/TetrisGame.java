45f4f0cbd814c7239f5923073f232ea13677d630
joao611 <joaofvde@gmail.com>
2017-05-25 18:17:30 +0100

    Removed some commented code

--- core/src/com/mygdx/tetris/TetrisGame.java
+++ core/src/com/mygdx/tetris/TetrisGame.java
@@ -29,15 +29,6 @@ public class TetrisGame extends Game {
 
 	/*
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
