7beafa911ce81c338e3286fc9785dbe121334bb6
Joao611 <joaofvde@gmail.com>
2017-06-11 14:05:13 +0100

    I don't even know, Android Studio keeps crashing here.

--- core/src/com/mygdx/tetris/logic/Block.java
+++ core/src/com/mygdx/tetris/logic/Block.java
@@ -3,55 +3,100 @@ package com.mygdx.tetris.logic;
 import com.badlogic.gdx.math.GridPoint2;
 
 /**
- * Created by up201505145 on 22/05/2017.
+ * A single block consisting of coordinates and an unique symbol.
  */
 public class Block implements Comparable {
     private GridPoint2 coords;
     private char symbol;
 
+    /**
+     * Creates a block with the given coordinates and symbol.
+     * @param x Horizontal coordinate. Must be >= 0.
+     * @param y Vertical coordinate. Must be >= 0.
+     * @param symbol Unique character.
+     */
     public Block(int x, int y, char symbol) {
         coords = new GridPoint2(x, y);
         this.symbol = symbol;
     }
 
+    /**
+     * Creates a block with the given coordinates and symbol.
+     * @param coords Pair of coordinates. Both must be >= 0.
+     * @param symbol Unique character.
+     */
     public Block(GridPoint2 coords, char symbol) {
         setCoords(coords);
         this.symbol = symbol;
     }
 
+    /**
+     * Changes the Block's coordinates.
+     * @param coords New pair of coordinates.
+     */
     public void setCoords(GridPoint2 coords) {
         this.coords = coords;
     }
 
+    /**
+     * Changes the Block's coordinates.
+     * @param x New horizontal coordinate.
+     * @param y New vertical coordinate.
+     */
     public void setCoords(int x, int y) {
         coords = new GridPoint2(x, y);
     }
 
+    /**
+     * Returns the Block's coordinates.
+     * @return Current pair of coordinates.
+     */
     public GridPoint2 getCoords() {
         return new GridPoint2(coords);
     }
 
+    /**
+     * Moves the Block down by 1 unit by decreasing y.
+     */
     public void moveDown() {
         coords.set(coords.x, coords.y - 1);
     }
 
+    /**
+     * Moves the Block left by 1 unit by decreasing x.
+     */
     public void moveLeft() {
         coords.set(coords.x - 1, coords.y);
     }
 
+    /**
+     * Moves the Block right by 1 unit by increasing x.
+     */
     public void moveRight() {
         coords.set(coords.x + 1, coords.y);
     }
 
+    /**
+     * Returns a copy of the Block with the same coordinates and symbol.
+     * @return New copied Block.
+     */
     public Object clone() {
         return new Block(coords, symbol);
     }
 
+    /**
+     * Returns the Block's symbol.
+     * @return Current symbol.
+     */
     public char getSymbol() {
         return symbol;
     }
 
-    // Ordered vertically for priority queue.
+    /**
+     * Ordered vertically for priority queue.
+     * @param o Block to compare to.
+     * @return 1 if this block has higher y or higher x with same y, 0 if same coordinates, -1 otherwise.
+     */
     @Override
     public int compareTo(Object o) {
         Block otherBlock = (Block) o;
@@ -69,7 +114,4 @@ public class Block implements Comparable {
             return -1;
         }
     }
-
-    // For priority queue ordering by vertical position.
-
 }
